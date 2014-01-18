11bb612052e8363ec696333d9ffdb57db93e472e
Yomguithereal <guillaumeplique@gmail.com>
2014-01-18 19:31:44 +0100

    Correcting expression

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-fuzzy "0.1.3"
-  :description "A handy compilation of algorithms dealing with fuzzy strings and phonetics."
+  :description "A handy collection of algorithms dealing with fuzzy strings and phonetics."
   :url "https://github.com/Yomguithereal/clj-fuzzy"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
