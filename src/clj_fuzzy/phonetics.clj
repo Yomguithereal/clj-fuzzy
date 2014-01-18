4647564509cc05562021b6d77453d7778ff0f5ee
Yomguithereal <guillaumeplique@gmail.com>
2014-01-18 19:05:32 +0100

    Adding caverphone to API

--- src/clj_fuzzy/phonetics.clj
+++ src/clj_fuzzy/phonetics.clj
@@ -9,8 +9,10 @@
 (ns clj-fuzzy.phonetics
   (:require [clj-fuzzy.metaphone :only [process]]
             [clj-fuzzy.soundex :only [process]]
-            [clj-fuzzy.nysiis :only [original]]))
+            [clj-fuzzy.nysiis :only [original]]
+            [clj-fuzzy.caverphone :only [process]]))
 
 (def metaphone clj-fuzzy.metaphone/process)
 (def soundex clj-fuzzy.soundex/process)
 (def nysiis clj-fuzzy.nysiis/original)
+(def caverphone clj-fuzzy.caverphone/process)
--- test/clj_fuzzy/api_test.clj
+++ test/clj_fuzzy/api_test.clj
@@ -6,7 +6,7 @@
 ;;   Author: PLIQUE Guillaume (Yomguithereal)
 ;;   Version: 0.1
 ;;
-(ns clj-fuzzy.api-test 
+(ns clj-fuzzy.api-test
   (:require [clojure.test :refer :all]
             [clj-fuzzy.phonetics :as p]
             [clj-fuzzy.metrics :as m]
@@ -15,7 +15,8 @@
 (deftest phonetics-test
   (is (= "TSKRMNXN" (p/metaphone "discrimination")))
   (is (= "A261" (p/soundex "Ashcroft")))
-  (is (= "ANDARSAN" (p/nysiis "Anderson"))))
+  (is (= "ANDARSAN" (p/nysiis "Anderson")))
+  (is (= "ANRKSN1111" (p/caverphone "Henricsson"))))
 
 (deftest metrics-test
   (is (= 2 (m/levensthein "book" "back")))
