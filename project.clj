ae32376ea8a9c69de7e3aa6bd15ed741dc5b6d93
Yomguithereal <guillaumeplique@gmail.com>
2014-02-11 21:19:45 +0100

    Providing JavaScript build

--- project.clj
+++ project.clj
@@ -28,5 +28,5 @@
      :crossover-path "src-cljs/clj_fuzzy",
      :compiler
      {:pretty-print false,
-      :output-to "target/cljs-fuzzy.min.js",
+      :output-to "clj-fuzzy.min.js",
       :optimizations :advanced}}]})
