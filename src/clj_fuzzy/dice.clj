eb90ce6b0e7943e04cf784f71f7e019a19a71fb9
Yomguithereal <guillaumeplique@gmail.com>
2014-04-28 10:26:58 +0100

    Refactoring dice coefficient

--- src/clj_fuzzy/dice.clj
+++ src/clj_fuzzy/dice.clj
@@ -9,23 +9,22 @@
 ;;     http://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
 ;;
 (ns clj-fuzzy.dice
-  (:require clojure.string)
-  (:use [clj-fuzzy.helpers :only [slice in?]]
-        [clojure.set :only [intersection]]))
+  (:require clojure.string
+            clojure.set)
+  (:use [clj-fuzzy.helpers :only [bigrams]]))
 
-(defn- pairing [word]
-  (map #(slice word % 2) (range 0 (dec (count word)))))
+;; Utilities
+(defn- letter-pairs
+  [string]
+  (bigrams (-> (clojure.string/replace string #"\s+" "")
+               (clojure.string/upper-case))))
 
-(defn- letter-pairs [string]
-  (let [ts (clojure.string/split (clojure.string/upper-case string) #"\s+")]
-    (flatten (map pairing ts))))
-
-;; OPTIMIZE: do we need to round up the result?
+;; Main functions
 (defn coefficient
   "Compute the Dice coefficient between two [strings]."
   [string1 string2]
   (let [p1 (letter-pairs string1)
         p2 (letter-pairs string2)
         sum (+ (count p1) (count p2))]
-    (/ (* 2.0 (count (intersection (set p1) (set p2))))
+    (/ (* 2.0 (count (clojure.set/intersection (set p1) (set p2))))
        sum)))
