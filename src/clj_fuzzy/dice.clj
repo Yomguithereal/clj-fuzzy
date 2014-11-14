0f9358578f011de3c9b13e6bf99dd5f9ada963f3
Yomguithereal <guillaumeplique@gmail.com>
2014-11-14 11:27:46 +0100

    Fix #25 - Dice coefficient edge cases The implementation will now return relevant coefficient on cases involving tiny strings containing less than two characters.

--- src/clj_fuzzy/dice.clj
+++ src/clj_fuzzy/dice.clj
@@ -6,7 +6,7 @@
 ;;   Author: PLIQUE Guillaume (Yomguithereal)
 ;;   Version: 0.1
 ;;   Explanation:
-;;     http://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
+;;     http://en.wikipedia.org/wiki/Sørensen–Dice_coefficient
 ;;
 (ns clj-fuzzy.dice
   (:require clojure.string
@@ -23,8 +23,11 @@
 (defn coefficient
   "Compute the Dice coefficient between two [strings]."
   [string1 string2]
-  (let [p1 (letter-pairs string1)
-        p2 (letter-pairs string2)
-        sum (+ (count p1) (count p2))]
-    (/ (* 2.0 (count (clojure.set/intersection p1 p2)))
-       sum)))
+  (cond (= string1 string2) 1.0
+        (and (< (count string1) 2)
+             (< (count string2) 2)) 0.0
+        :else (let [p1 (letter-pairs string1)
+                    p2 (letter-pairs string2)
+                    sum (+ (count p1) (count p2))]
+                (/ (* 2.0 (count (clojure.set/intersection p1 p2)))
+                   sum))))
