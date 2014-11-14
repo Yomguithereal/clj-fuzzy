c1bab4e15c87705a556d53a7096c6e64a31a37ed
Yomguithereal <guillaumeplique@gmail.com>
2014-11-14 18:16:22 +0100

    Adding basic lancaster stemmer unit tests

--- test/clj_fuzzy/lancaster_test.clj
+++ test/clj_fuzzy/lancaster_test.clj
@@ -10,5 +10,25 @@
   (:require [clojure.test :refer :all]
             [clj-fuzzy.lancaster :refer :all]))
 
+(def tests
+  ['("worker" "work")
+   '("marks" "mark")
+   '("MARKS" "mark")
+   '("living" "liv")
+   '("thing" "thing")
+   '("ear" "ear")
+   '("string" "string")
+   '("triplicate" "triply")
+   '("classified" "class")
+   '("maximum" "maxim")
+   '("presumably" "presum")
+   '("exceed" "excess")
+   '("anguish" "anct")
+   '("affluxion" "affluct")
+   '("discept" "disceiv")])
+
 (deftest stem-test
-  (is (= "work" (stem "worker"))))
+  (doseq [pair tests]
+    (let [original-word (first pair)
+          stemmed-word (second pair)]
+      (is (= stemmed-word (stem original-word))))))
