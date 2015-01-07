fbaf0439f5d9b14286ae184cf61aebb9b7f95191
Yomguithereal <guillaumeplique@gmail.com>
2015-01-07 19:59:37 +0100

    Improving levenshtein performance. Related to #33

--- src/clj_fuzzy/levenshtein.clj
+++ src/clj_fuzzy/levenshtein.clj
@@ -5,20 +5,25 @@
 ;;
 ;;   Author: PLIQUE Guillaume (Yomguithereal)
 ;;   Version: 0.1
-;;   Source:
-;;     http://www.learningclojure.com/2010/11/
-;;            levenshtein-distance-edit-distance.html
+;;   Source: https://gist.github.com/vishnuvyas/958488
 ;;
 (ns clj-fuzzy.levenshtein)
+
+(defn- next-row
+  [previous current other-seq]
+  (reduce
+    (fn [row [diagonal above other]]
+      (let [update-val (if (= other current)
+                          diagonal
+                          (inc (min diagonal above (peek row))))]
+        (conj row update-val)))
+    [(inc (first previous))]
+    (map vector previous (next previous) other-seq)))
 
 (defn distance
   "Compute the levenshtein distance between two [sequences]."
   [sequence1 sequence2]
-  (cond
-    (empty? sequence1) (count sequence2)
-    (empty? sequence2) (count sequence1)
-    :else (min
-            (+ (if (= (first sequence1) (first sequence2)) 0 1)
-                (distance (rest sequence1) (rest sequence2)))
-            (inc (distance (rest sequence1) sequence2))
-            (inc (distance sequence1 (rest sequence2))))))
+  (peek
+    (reduce (fn [previous current] (next-row previous current sequence2))
+            (map #(identity %2) (cons nil sequence2) (range))
+            sequence1)))
