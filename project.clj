7bc1d4ec898d6da6625fbe3115aa624f6f602293
Yomguithereal <guillaumeplique@gmail.com>
2015-07-27 11:31:55 +0200

    Bump 0.3.1 oops

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-fuzzy "0.3.0"
+(defproject clj-fuzzy "0.3.1"
   :description "A handy collection of algorithms dealing with fuzzy strings and phonetics."
   :url "http://yomguithereal.github.io/clj-fuzzy/"
   :license {:name "MIT"
