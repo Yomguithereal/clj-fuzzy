7bd29ee8f2a1bf71f934ea9ae25724f00cdfedae
Yomguithereal <guillaumeplique@gmail.com>
2015-07-27 10:53:53 +0200

    Switching to clojure 1.7

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://yomguithereal.github.io/clj-fuzzy/"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "0.0-3308"]]
   :plugins [[lein-kibit "0.0.8"]
             [lein-cljsbuild "1.0.6"]]
