d6783bab16c87db9d7fddb15fec2c896b805d5ae
Yomguithereal <guillaumeplique@gmail.com>
2016-09-09 11:40:45 +0200

    Related to #39 - clojurescript to dev deps

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clj-fuzzy "0.3.2"
+(defproject clj-fuzzy "0.3.3"
   :description "A handy collection of algorithms dealing with fuzzy strings and phonetics."
   :url "http://yomguithereal.github.io/clj-fuzzy/"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "0.0-3308"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "0.0-3308"]]}}
   :plugins [[lein-kibit "0.0.8"]
             [lein-cljsbuild "1.0.6"]]
   :cljsbuild {
